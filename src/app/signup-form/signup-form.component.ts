ce0e1699c205fa6c739e85701fd56dc3816885a0
Immanuel Paul <immanuel.paul@ameexusa.com>
2021-04-01 13:54:32 +0530

    SignUp Form submission

--- src/app/signup-form/signup-form.component.ts
+++ src/app/signup-form/signup-form.component.ts
@@ -1,19 +1,40 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { HttpDataService } from '../http-data.service';
 
 @Component({
   selector: 'mbs-signup-form',
   templateUrl: './signup-form.component.html',
   styleUrls: ['./signup-form.component.css']
 })
+
+
 export class SignupFormComponent implements OnInit {
 
-  constructor() { }
+  @ViewChild('f', {static: false}) signupForm: NgForm;
+
+  user = {
+    userName: '',
+    user_Email: '',
+    user_Fname: '',
+    user_Lname: '',
+    user_password: '',
+  }
+
+  constructor(private http: HttpDataService) { }
 
   ngOnInit(): void {
   }
 
   onSubmit(): void {
-    console.log('Submitted!');
+    this.user.userName = this.signupForm.value.username;
+    this.user.user_Email = this.signupForm.value.email;
+    this.user.user_Fname = this.signupForm.value.firstName;
+    this.user.user_Lname = this.signupForm.value.lastName;
+    this.user.user_password = this.signupForm.value.password;
+    this.http.postData('user', this.user).subscribe(data => {
+      console.log('UserInformation submitted: ', data);
+    });
   }
 
   onReset() {
